4e4ee69c530191bb435b904bab5304d35554606e
ilya-shylkou <ilia3538@gmail.com>
2025-01-17 07:42:20 +0100

    formated tests for review

--- src/test/java/com/circlek/Test01.java
+++ src/test/java/com/circlek/Test01.java
@@ -18,11 +18,15 @@ public class Test01 {
         driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
 
         //assert Remember Me Button
+
         WebElement rememeberMeLabel = driver.findElement(By.id("remember_me_label"));
+
         assertEquals("Remember Me", rememeberMeLabel.getText());
 
         //assert Sign In text
+
         WebElement signInButton = driver.findElement(By.className("login"));
+
         assertEquals("Sign In", signInButton.getText());
     }
 
@@ -33,10 +37,14 @@ public class Test01 {
         driver.get("https://circlek-public.github.io/cki-service-recruitment-task/");
 
         //click Remember Me checkbox
+
         WebElement rememberMeCheckBox = driver.findElement(By.xpath("//*[contains(text(), \"Remember Me\")]/../input"));
+
         rememberMeCheckBox.click();
 
+
         //check if clicked
+
         assertEquals(rememberMeCheckBox.isSelected(), true);
 
     }
